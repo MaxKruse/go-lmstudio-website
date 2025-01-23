e2791b3a77bec62feab3b33d820d459af10b8350
Maximilian Kruse <maxkruse1996@gmail.com>
2025-01-23 12:52:26 +0100

    Add annotation for server main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,11 +9,24 @@ import (
 	_ "github.com/maxkruse/go-lmstudio-website/docs"
 )
 
+// @title LM Studio Website API
+// @version 1.0
+// @description This is a sample server for a bookstore server with included LLM functionality.
+
+// @contact.name None
+// @contact.url None
+// @contact.email None
+
+// @license.name None
+// @license.url None
+
+// @host bookstore.mkruse.xyz
+// @BasePath /v1
 func main() {
 	e := echo.New()
 	e.GET("/swagger/*", echoSwagger.WrapHandler)
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(":3000"))
 }
