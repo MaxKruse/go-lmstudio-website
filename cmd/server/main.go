0640aa70f80d7e3df744d65c61cbcd9e906f3a01
Maximilian Kruse <maxkruse1996@gmail.com>
2025-01-23 12:34:57 +0100

    Add doc stub

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,10 +4,14 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	echoSwagger "github.com/swaggo/echo-swagger"
+
+	_ "github.com/maxkruse/go-lmstudio-website/cmd/server/docs"
 )
 
 func main() {
 	e := echo.New()
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
